5008a60a0a26f092e539126c00969a5f9c05db82
Alex Dergach <C20401562@mytudublin.com>
2022-04-27 16:10:52 +0100

    Created Nematode Class, private feilds and getters and setters

---
+++ java/src/ie/tudublin/Nematode.java
@@ -0,0 +1,55 @@
+package ie.tudublin;
+
+import processing.data.TableRow;
+
+public class Nematode
+{
+    private String name;
+    private int length;
+    private int limbs;
+    private char gender;
+    private int eyes;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        this.length = length;
+    }
+    public int getLimbs() {
+        return limbs;
+    }
+    public void setLimbs(int limbs) {
+        this.limbs = limbs;
+    }
+    public char getGender() {
+        return gender;
+    }
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+    public int getEyes() {
+        return eyes;
+    }
+    public void setEyes(int eyes) {
+        this.eyes = eyes;
+    }
+
+    public Nematode(String name, int length, int limbs, char gender, int eyes)
+    {
+        this.name = name;
+        this.length = length;
+        this.limbs = limbs;
+        this.gender = gender;
+        this.eyes = eyes;
+
+    }
+    
+
+}
